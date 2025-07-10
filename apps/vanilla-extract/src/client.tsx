1799e2cd2f77135c027a25d6e7b69c3de8824846
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-07-10 23:04:03 +0200

    Don't use relative imports

--- apps/vanilla-extract/src/client.tsx
+++ apps/vanilla-extract/src/client.tsx
@@ -1,5 +1,5 @@
 "use client";
 
-import { clientContainer } from "./client-style.css.js";
+import { clientContainer } from "#src/client-style.css.js";
 
 export const ClientCounter = () => <div className={clientContainer}>test-client</div>;

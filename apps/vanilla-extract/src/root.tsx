379db6fd385e0cbe9ff8933872ce6b20263cadef
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-11 05:54:35 +0900

    fix: add `loadCss()` manually (#1)
    * fix: add `loadCss()` manually
    
    * fix: invalidate client vanilla css on server component css change
    
    * fix: `transformRequest` first
    
    * chore: cleanup

--- apps/vanilla-extract/src/root.tsx
+++ apps/vanilla-extract/src/root.tsx
@@ -1,6 +1,7 @@
 import { container } from "#src/styles.css.js";
+import { ClientCounter } from "./client.js";
 
-const App = () => <h1 className={container}>Test</h1>;
+const App = () => <h1 className={container}>Test-server</h1>;
 
 export const Root = () => (
 	<html lang="en">
@@ -9,9 +10,11 @@ export const Root = () => (
 			<link href="/vite.svg" rel="icon" type="image/svg+xml" />
 			<meta content="width=device-width, initial-scale=1.0" name="viewport" />
 			<title>Vite + RSC</title>
+			{import.meta.viteRsc.loadCss()}
 		</head>
 		<body>
 			<App />
+			<ClientCounter />
 		</body>
 	</html>
 );
--- apps/vanilla-extract/vite.config.ts
+++ apps/vanilla-extract/vite.config.ts
@@ -10,5 +10,35 @@ export default defineConfig({
 		rsc: { build: { rollupOptions: { input: { index: "./src/framework/entry.rsc.tsx" } } } },
 		ssr: { build: { rollupOptions: { input: { index: "./src/framework/entry.ssr.tsx" } } } },
 	},
-	plugins: [rsc(), react(), vanillaExtractPlugin(), inspect()],
+	plugins: [
+		// workaround?
+		// https://github.com/vanilla-extract-css/vanilla-extract/blob/c907c109496635beec363418d1d341deb41c47c0/packages/vite-plugin/src/index.ts#L245
+		{
+			name: "fix-vanilla-extract",
+			async hotUpdate(ctx) {
+				if (this.environment.name === "rsc") {
+					if (ctx.file.includes(".css.")) {
+						const mods = ctx.modules[0]?.importedModules;
+						const clientEnvironment = ctx.server.environments.client;
+						for (const mod of mods ?? []) {
+							if (mod.file?.endsWith(".vanilla.css")) {
+								const clientMods = clientEnvironment.moduleGraph.getModulesByFile(mod.file);
+								for (const clientMod of clientMods ?? []) {
+									// Originally `xxx.css.ts` doesn't exist in client environment
+									// since server style system directly pass `xxx.css.ts.virtual.css` to client.
+									// Here we ensure it's processed on client so that vanilla-extract HMR works.
+									await clientEnvironment.transformRequest(ctx.file);
+									clientEnvironment.reloadModule(clientMod);
+								}
+							}
+						}
+					}
+				}
+			},
+		},
+		rsc(),
+		react(),
+		vanillaExtractPlugin(),
+		inspect(),
+	],
 });

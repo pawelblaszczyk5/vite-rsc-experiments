774a5d136e4c6bf8651aee0f5d7413855406797c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-11 05:54:35 +0900

    fix: add `loadCss()` manually (#1)
    * fix: add `loadCss()` manually
    
    * fix: invalidate client vanilla css on server component css change
    
    * fix: `transformRequest` first
    
    * chore: cleanup

--- apps/vanilla-extract/src/client.tsx
+++ apps/vanilla-extract/src/client.tsx
@@ -1,5 +1,5 @@
 "use client";
 
-import { container } from "#src/styles.css.js";
+import { clientContainer } from "./client-style.css.js";
 
-export const ClientCounter = () => <div className={container}>test</div>;
+export const ClientCounter = () => <div className={clientContainer}>test-client</div>;

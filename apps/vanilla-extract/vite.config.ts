7372822488337c68493ccf2c89e4b447972d5976
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-10 18:59:23 +0900

    fix: `transformRequest` first

--- apps/vanilla-extract/vite.config.ts
+++ apps/vanilla-extract/vite.config.ts
@@ -15,21 +15,22 @@ export default defineConfig({
 		// https://github.com/vanilla-extract-css/vanilla-extract/blob/c907c109496635beec363418d1d341deb41c47c0/packages/vite-plugin/src/index.ts#L245
 		{
 			name: "fix-vanilla-extract",
-			hotUpdate(ctx) {
+			async hotUpdate(ctx) {
 				if (this.environment.name === "rsc") {
 					if (ctx.file.includes(".css.")) {
 						const mods = ctx.modules[0]?.importedModules;
 						const clientEnvironment = ctx.server.environments.client;
 						for (const mod of mods ?? []) {
 							if (mod.file?.endsWith(".vanilla.css")) {
-								this.environment.moduleGraph.invalidateModule(mod);
 								const clientMods = clientEnvironment.moduleGraph.getModulesByFile(mod.file);
 								for (const clientMod of clientMods ?? []) {
-									clientEnvironment.moduleGraph.invalidateModule(clientMod);
+									// Originally `xxx.css.ts` doesn't exist in client environment
+									// since server style system directly pass `xxx.css.ts.virtual.css` to client.
+									// Here we ensure it's processed on client so that vanilla-extract HMR works.
+									await clientEnvironment.transformRequest(ctx.file);
+									clientEnvironment.reloadModule(clientMod);
 								}
 							}
-							// TODO: css hmr not working, so for now full-reload
-							clientEnvironment.hot.send({ type: "full-reload" });
 						}
 					}
 				}

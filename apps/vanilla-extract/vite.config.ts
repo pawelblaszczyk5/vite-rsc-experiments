9febc681be9d53a4911bdc6e72e80eb06f6609e4
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-07-10 23:00:57 +0200

    Small style/linter fixes

--- apps/vanilla-extract/vite.config.ts
+++ apps/vanilla-extract/vite.config.ts
@@ -11,30 +11,36 @@ export default defineConfig({
 		ssr: { build: { rollupOptions: { input: { index: "./src/framework/entry.ssr.tsx" } } } },
 	},
 	plugins: [
-		// workaround?
+		// eslint-disable-next-line no-secrets/no-secrets -- that's link with commit hash to specific place which probably breaks the plugin and why this fix is needed
 		// https://github.com/vanilla-extract-css/vanilla-extract/blob/c907c109496635beec363418d1d341deb41c47c0/packages/vite-plugin/src/index.ts#L245
 		{
-			name: "fix-vanilla-extract",
-			async hotUpdate(ctx) {
-				if (this.environment.name === "rsc") {
-					if (ctx.file.includes(".css.")) {
-						const mods = ctx.modules[0]?.importedModules;
-						const clientEnvironment = ctx.server.environments.client;
-						for (const mod of mods ?? []) {
-							if (mod.file?.endsWith(".vanilla.css")) {
-								const clientMods = clientEnvironment.moduleGraph.getModulesByFile(mod.file);
-								for (const clientMod of clientMods ?? []) {
-									// Originally `xxx.css.ts` doesn't exist in client environment
-									// since server style system directly pass `xxx.css.ts.virtual.css` to client.
-									// Here we ensure it's processed on client so that vanilla-extract HMR works.
-									await clientEnvironment.transformRequest(ctx.file);
-									clientEnvironment.reloadModule(clientMod);
-								}
-							}
-						}
+			async hotUpdate(context) {
+				if (this.environment.name !== "rsc" || !context.file.includes(".css.")) {
+					return;
+				}
+
+				const mods = context.modules[0]?.importedModules;
+				const clientEnvironment = context.server.environments.client;
+
+				if (!mods) {
+					return;
+				}
+
+				for (const mod of mods) {
+					if (!mod.file?.endsWith(".vanilla.css")) {
+						return;
+					}
+
+					const clientMods = clientEnvironment.moduleGraph.getModulesByFile(mod.file);
+
+					for (const clientModule of clientMods ?? []) {
+						// Originally `xxx.css.ts` doesn't exist in client environment since server style system directly pass `xxx.css.ts.virtual.css` to client. Here we ensure it's processed on client so that vanilla-extract HMR works.
+						await clientEnvironment.transformRequest(context.file);
+						await clientEnvironment.reloadModule(clientModule);
 					}
 				}
 			},
+			name: "fix-vanilla-extract",
 		},
 		rsc(),
 		react(),

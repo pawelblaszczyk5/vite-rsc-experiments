59902736c4cbb452a483fb6ea43795437a5c54c2
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-10 18:45:14 +0900

    fix: invalidate client vanilla css on server component css change

--- apps/vanilla-extract/vite.config.ts
+++ apps/vanilla-extract/vite.config.ts
@@ -10,5 +10,34 @@ export default defineConfig({
 		rsc: { build: { rollupOptions: { input: { index: "./src/framework/entry.rsc.tsx" } } } },
 		ssr: { build: { rollupOptions: { input: { index: "./src/framework/entry.ssr.tsx" } } } },
 	},
-	plugins: [rsc(), react(), vanillaExtractPlugin(), inspect()],
+	plugins: [
+		// workaround?
+		// https://github.com/vanilla-extract-css/vanilla-extract/blob/c907c109496635beec363418d1d341deb41c47c0/packages/vite-plugin/src/index.ts#L245
+		{
+			name: "fix-vanilla-extract",
+			hotUpdate(ctx) {
+				if (this.environment.name === "rsc") {
+					if (ctx.file.includes(".css.")) {
+						const mods = ctx.modules[0]?.importedModules;
+						const clientEnvironment = ctx.server.environments.client;
+						for (const mod of mods ?? []) {
+							if (mod.file?.endsWith(".vanilla.css")) {
+								this.environment.moduleGraph.invalidateModule(mod);
+								const clientMods = clientEnvironment.moduleGraph.getModulesByFile(mod.file);
+								for (const clientMod of clientMods ?? []) {
+									clientEnvironment.moduleGraph.invalidateModule(clientMod);
+								}
+							}
+							// TODO: css hmr not working, so for now full-reload
+							clientEnvironment.hot.send({ type: "full-reload" });
+						}
+					}
+				}
+			},
+		},
+		rsc(),
+		react(),
+		vanillaExtractPlugin(),
+		inspect(),
+	],
 });
